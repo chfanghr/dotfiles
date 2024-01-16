d62c17833b2c212900ff3c5059e829fecb0eb6b8
Hongrui Fang <chfanghr@gmail.com>
2024-01-16 19:40:25 +0800

    generate nix store key

---
+++ profiles/darwin/generate-nix-cache-key.nix
@@ -0,0 +1,13 @@
+{pkgs, config, ...}: {
+  launchd.daemons.generate-nix-cache-key = {
+    serviceConfig.LaunchOnlyOnce = true;
+    path = [ pkgs.nix ];
+    script = ''
+      [[ -f /etc/nix/private-key ]] && exit
+      nix-store --generate-binary-cache-key ${config.networking.hostName}-1 /etc/nix/private-key /etc/nix/public-key
+    '';
+  };
+  nix.extraOptions = ''
+    secret-key-files = /etc/nix/private-key
+  '';
+}
--- profiles/darwin/nix.nix
+++ profiles/darwin/nix.nix
@@ -38,4 +38,6 @@
       enable = true;
     };
   };
+
+  imports = [ ./generate-nix-cache-key.nix ];
 }
