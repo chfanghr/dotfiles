0b09925e7085f2d6e22751254e994f6ebc1e4d00
Hongrui Fang <chfanghr@gmail.com>
2024-04-27 13:46:33 +0800

    bump inputs; use unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Hongrui Fang's machine configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin/master";
@@ -10,7 +10,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
