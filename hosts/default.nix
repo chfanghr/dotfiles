d958a77a435fcb2b22fae1b3f848192bfdebdc4a
Hongrui Fang <chfanghr@gmail.com>
2024-05-21 18:38:38 +0800

    fuck

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,6 +1,24 @@
-{
-  imports = [
-    ./Hera.nix
-    ./Hermes.nix
-  ];
+{inputs, ...}: {
+  flake.darwinConfigurations = {
+    Hera = inputs.nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      specialArgs = {inherit inputs;};
+      modules = [
+        {
+          imports = [../profiles/darwin];
+          darwin.hostName = "Hera";
+        }
+      ];
+    };
+    Hermes = inputs.nix-darwin.lib.darwinSystem {
+      system = "x86_64-darwin";
+      specialArgs = {inherit inputs;};
+      modules = [
+        {
+          imports = [../profiles/darwin];
+          darwin.hostName = "Hermes";
+        }
+      ];
+    };
+  };
 }
